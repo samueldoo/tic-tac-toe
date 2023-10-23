a3dd9e1384f18d215c81d1d35a2def099c14d0c0
Alademehin Samuel <alademahinsamuel@yahoo.com>
2023-10-23 23:11:57 +0100

    update

--- src/components/TicTacToe/TicTacToe.jsx
+++ src/components/TicTacToe/TicTacToe.jsx
@@ -94,7 +94,7 @@ won(data[2])
     }
   return (
     <div className='container'>
-        <h1 className='title' ref={titleRef}>Tic Tac Toe Game In <span>React</span></h1>
+        <h1 className='title' ref={titleRef}>Tic Tac Toe Game In <span>Game</span></h1>
         <div className="board">
             <div className="row1">
                 <div className="boxes" ref={box1} onClick={(e) => {toggle(e, 0)}}></div>
